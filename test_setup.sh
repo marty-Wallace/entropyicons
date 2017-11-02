8c36fcb68dce37e10ff227be731c459868f360b3
Martin Wallace <martin.v.wallace@ieee.org>
2017-11-02 15:44:39 -0700

    Fix issues with clippy

--- test_setup.sh
+++ test_setup.sh
@@ -3,7 +3,9 @@
 echo "DATABASE_URL=postgres://postgres@localhost/rust_360" > .env
 source ./.env
 cargo install diesel_cli
-cargo install clippy
+if [[ "$1" == nightly* ]]; then
+    cargo install clippy
+fi
 diesel database reset
 if [ -f "./db_setup" ]; then
     psql -U postgres rust_360 < db_setup
