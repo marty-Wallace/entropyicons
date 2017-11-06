93762f4c03d72f53dae3a2c94dff268e3ca75266
Martin Wallace <Martin.V.Wallace@ieee.org>
2017-11-06 13:11:03 -0800

    Dev/rocket (#2)
    Add in Rocket framework, restructure files, add tests

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
+
 extern crate rust_360;
-use rust_360::{ create_post, establish_connection };
+use rust_360::server::rocket;
 
 fn main() {
-
-    let s: &str = "Post 1";
-    let b = "This is the body of post 1";
-
-    let conn = establish_connection();
-    create_post(&conn, s, b);
-
+    rocket().launch();
 }
-
